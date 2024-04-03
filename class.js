94e7e17d8456bc82a2d0ec2b10d333c3f5edfd85
kananbayramli <bayramli-kenan@mail.ru>
2024-04-03 16:13:20 +0400

    object created

--- class.js
+++ class.js
@@ -14,3 +14,15 @@ var Bus = /** @class */ (function () {
     };
     return Bus;
 }());
+var taxi_1 = new Taxi(); //object created
+taxi_1.travelTo({ x: 1, y: 2 });
+taxi_1.currentLocation = { x: 2, y: 4 };
+var taxi_2 = new Taxi(); //object created
+taxi_2.travelTo({ x: 6, y: 7 });
+taxi_2.currentLocation = { x: 2, y: 5 };
+var bus_1 = new Bus();
+bus_1.currentLocation = { x: 3, y: 4 };
+console.log(taxi_1.currentLocation.x);
+console.log(taxi_1.currentLocation.y);
+console.log(taxi_2.currentLocation.x);
+console.log(taxi_2.currentLocation.x);
